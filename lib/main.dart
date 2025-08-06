f49a3ea1d91913bd016787bb14779edfb0d480ff
Pham Viet Toan <toanpham622002@gmail.com>
2025-08-06 15:11:03 +0700

    Supabase update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -10,13 +11,16 @@ import 'screens/home.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isLoggedIn = prefs.getBool('stayLoggedIn') ?? false;
+
   await Supabase.initialize(
-    url: 'https://knvnlqxpcrcgdydhcvma.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtudm5scXhwY3JjZ2R5ZGhjdm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MDc0NDAsImV4cCI6MjA2OTE4MzQ0MH0.L_n5uIS_YUKfNn6qAti_EuvvIXxT8X-hBe3jgA5iMIY',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+
   runApp(ProviderScope(child: MyApp(isLoggedIn: isLoggedIn)));
 }
 
